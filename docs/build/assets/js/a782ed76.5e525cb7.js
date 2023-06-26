a6ee59f576909edfb95b44d18200b499c017ae7b
tunn3l <maxrademacher02@gmail.com>
2023-06-26 22:19:03 +0200

    removed accidental build push

--- docs/build/assets/js/a782ed76.5e525cb7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkubefuzz=self.webpackChunkkubefuzz||[]).push([[982],{3769:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docs/build/assets/js/ba41f007.2c4fd99b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkubefuzz=self.webpackChunkkubefuzz||[]).push([[118],{5745:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
