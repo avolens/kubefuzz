ae9cf17728ce808ca6b341d83d4db91841e01cc2
tunn3l <maxrademacher02@gmail.com>
2023-06-27 12:15:17 +0200

    silenced some modules in modules

--- src/log/mod.rs
+++ src/log/mod.rs
@@ -13,9 +13,12 @@ pub fn initlog() {
     match env::var("RUST_LOG") {
         Ok(_) => {
             let curlevel = env::var("RUST_LOG").unwrap();
-            env::set_var("RUST_LOG", format!("{},kube=off", curlevel));
+            env::set_var(
+                "RUST_LOG",
+                format!("{},kube_client=off,tower=off,hyper=off", curlevel),
+            );
         }
-        Err(_) => env::set_var("RUST_LOG", "info,kube=off"),
+        Err(_) => env::set_var("RUST_LOG", "info,kube_client=off,tower=off,hyper=off"),
     }
     pretty_env_logger::init();
 }
